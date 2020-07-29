92dd9adda9a7e053618b5c1f6085a340e17e39ac
Krishna M <mvrkrishna@gmail.com>
2020-07-29 13:30:53 +0530

    Elastic Beanstalk application

---
+++ src/config/config.ts
@@ -0,0 +1,17 @@
+export const config = {
+  "dev": {
+    "username": "mvrkrishna",
+    "password": "udagramkrishna",
+    "database": "udagramkrishna",
+    "host": "udagramkrishna.cbg90btydhzm.us-east-1.rds.amazonaws.com",
+    "dialect": "postgres",
+  },
+  "aws":{
+    "aws_region": "us-east-1",
+    "aws_profile": "default",
+    "aws_media_bucket": "udagram-krishna-dev"
+  },
+  "jwt":{
+    "secret":"Helloworld"
+  }
+}
